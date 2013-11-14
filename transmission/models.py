8d85bf4770a9032e5530ddd52db030e9cef2e982
Igor Shishkin <me@teran.ru>
2013-11-14 10:38:51 +0400

    Added File and Hardlink models, __unicode__ methods added to all classes

--- transmission/models.py
+++ transmission/models.py
@@ -4,7 +4,28 @@ class Group(models.Model):
     name = models.CharField(max_length=255)
     color = models.CharField(max_length=7)
 
+    def __unicode__(self):
+        return self.name
+
 class Torrent(models.Model):
     tid = models.IntegerField()
     name = models.CharField(max_length=255)
     group = models.ForeignKey(Group, related_name='torrents', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class File(models.Model):
+    filename = models.CharField(max_length=255)
+    torrent = models.ForeignKey(Torrent, related_name='files')
+
+    def __unicode__(self):
+        return self.filename
+
+class Hardlink(models.Model):
+    token = models.CharField(max_length=64)
+    file = models.ForeignKey(File, related_name='hardlinks')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.token
