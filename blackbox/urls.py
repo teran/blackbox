69173c6b4559a4944a50a5474c2c9872e9aeb78b
Igor Shishkin <me@teran.ru>
2013-11-17 12:07:04 +0400

    cleanup

--- blackbox/urls.py
+++ blackbox/urls.py
@@ -11,15 +11,5 @@ urlpatterns = patterns(
     (r'^api\/transmission\/add$', 'transmission.views.api_add_torrent'),
     (r'^api\/transmission\/(?P<id>[0-9]+)\/' +
      '(?P<action>start|stop|delete|info|verify)$',
-     'transmission.views.api_action'),
-
-    # Examples:
-    # url(r'^$', 'blackbox.views.home', name='home'),
-    # url(r'^blackbox/', include('blackbox.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     'transmission.views.api_action')
 )
