651411a0cac6e6c784c35eb0e40d13389d13bf08
Igor Shishkin <me@teran.ru>
2013-12-02 14:29:20 +0400

    Fix for urls regex’es

--- blackbox/urls.py
+++ blackbox/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     (r'^$', 'transmission.views.index'),
-    (r'^api\/transmission\/filter', 'transmission.views.api_filter'),
-    (r'^api\/transmission\/list', 'transmission.views.api_list'),
+    (r'^api\/transmission\/filter$', 'transmission.views.api_filter'),
+    (r'^api\/transmission\/list$', 'transmission.views.api_list'),
     (r'^api\/transmission\/add$', 'transmission.views.api_add_torrent'),
     (r'^api\/transmission\/(?P<hash>[0-9a-f]{40})\/' +
      '(?P<action>start|stop|delete|info|verify)$',
