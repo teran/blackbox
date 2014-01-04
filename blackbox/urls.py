2d15b8d85734bad3475c0f0de90e18fbf7c2ec16
Igor Shishkin <me@teran.ru>
2014-01-04 16:12:31 +0400

    fix for hardlink method in urls.py

--- blackbox/urls.py
+++ blackbox/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns(
     (r'^api\/transmission\/list$', 'transmission.views.api_list'),
     (r'^api\/transmission\/add$', 'transmission.views.api_add_torrent'),
     (r'^api\/hardlink\/(?P<file>[0-9]+)$',
-     'transmission.views.api_hardlink'),
+     'transmission.views.hardlink'),
     (r'^api\/transmission\/(?P<hash>[0-9a-f]{40})\/' +
      '(?P<action>start|stop|delete|info|verify)$',
      'transmission.views.api_action'),
