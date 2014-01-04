eef2fd6198e849056f11bd8b7bf922c86f68ba6c
Igor Shishkin <me@teran.ru>
2014-01-04 16:10:31 +0400

    Optimized imports, reimplemented hardlink method, some improvements

--- transmission/views.py
+++ transmission/views.py
@@ -1,3 +1,12 @@
+import transmissionrpc
+import tempfile
+from base64 import b64encode
+from json import dumps
+from os import unlink, path
+from hashlib import sha1
+from random import random
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -6,59 +15,7 @@ from django.shortcuts import HttpResponse, render_to_response, \
     RequestContext, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
-import transmissionrpc
-import tempfile
-from base64 import b64encode
-from json import dumps
-from os import unlink, symlink, path
-from hashlib import sha1
-from random import random
-from datetime import datetime, timedelta
-
 from transmission.models import Torrent, Group, File, Hardlink
-
-
-@login_required
-def api_add_torrent(request):
-    """
-    Add torrent with a torrent file, http link or magnet link
-    """
-    if request.method == 'POST':
-        tc = transmissionrpc.Client(
-            settings.TRANSMISSION['default']['HOST'],
-            port=settings.TRANSMISSION['default']['PORT'],
-            user=settings.TRANSMISSION['default']['USER'],
-            password=settings.TRANSMISSION['default']['PASSWORD'])
-
-        if 'torrent' in request.FILES:
-            with tempfile.NamedTemporaryFile('w+b', delete=False) as fp:
-                f = request.FILES.get('torrent')
-                tf = fp.name
-                for chunk in f.chunks():
-                    fp.write(chunk)
-
-            fp.close()
-            fp = open(tf, 'r')
-
-            tc.add_torrent(b64encode(fp.read()))
-
-            fp.close()
-            unlink(tf)
-        else:
-            for url in request.POST['torrentUrls'].split("\n"):
-                try:
-                    tc.add_torrent(url)
-                except:
-                    pass
-
-        return redirect('/')
-    else:
-        return HttpResponseBadRequest(
-            content=dumps({
-                'status': 'error',
-                'reason': 'Request method should be POST'
-            }), content_type='application/json'
-        )
 
 
 @csrf_exempt
@@ -143,47 +100,46 @@ def api_action(request, hash, action):
 
 
 @login_required
-def api_list(request):
+def api_add_torrent(request):
     """
-    List all torrents
+    Add torrent with a torrent file, http link or magnet link
     """
-    tc = transmissionrpc.Client(
-        settings.TRANSMISSION['default']['HOST'],
-        port=settings.TRANSMISSION['default']['PORT'],
-        user=settings.TRANSMISSION['default']['USER'],
-        password=settings.TRANSMISSION['default']['PASSWORD'])
+    if request.method == 'POST':
+        tc = transmissionrpc.Client(
+            settings.TRANSMISSION['default']['HOST'],
+            port=settings.TRANSMISSION['default']['PORT'],
+            user=settings.TRANSMISSION['default']['USER'],
+            password=settings.TRANSMISSION['default']['PASSWORD'])
 
-    filter = request.GET.get('filter', '')
+        if 'torrent' in request.FILES:
+            with tempfile.NamedTemporaryFile('w+b', delete=False) as fp:
+                f = request.FILES.get('torrent')
+                tf = fp.name
+                for chunk in f.chunks():
+                    fp.write(chunk)
 
-    rpclist = tc.get_torrents()
-    data = {}
-    for t in rpclist:
-        torrent, created = Torrent.objects.get_or_create(
-            hash=t.hashString
+            fp.close()
+            fp = open(tf, 'r')
+
+            tc.add_torrent(b64encode(fp.read()))
+
+            fp.close()
+            unlink(tf)
+        else:
+            for url in request.POST['torrentUrls'].split("\n"):
+                try:
+                    tc.add_torrent(url)
+                except:
+                    pass
+
+        return redirect('/')
+    else:
+        return HttpResponseBadRequest(
+            content=dumps({
+                'status': 'error',
+                'reason': 'Request method should be POST'
+            }), content_type='application/json'
         )
-        if created:
-            torrent.name = t.name
-            torrent.save()
-
-        data[t.hashString] = {
-            'hash': t.hashString,
-            'name': t.name,
-            'status': t.status,
-            'progress': t.progress,
-            'recheckProgress': t.recheckProgress * 100,
-        }
-
-    for t in Torrent.objects.all():
-        try:
-            data[t.hash]['name'] = t.name
-        except KeyError:
-            t.delete()
-            del(data[t.hash])
-
-    return HttpResponse(
-        content=dumps(data),
-        content_type='application/json'
-    )
 
 
 @login_required
@@ -226,55 +182,48 @@ def api_filter(request):
 
 
 @login_required
-def api_hardlink(request, file, static=False):
+def api_list(request):
     """
-    Create hardlink to file
+    List all torrents
     """
-    expired = Hardlink.objects.filter(
-        created__lte = (datetime.now() - timedelta(
-            seconds=settings.HARDLINK_TTL)))
+    tc = transmissionrpc.Client(
+        settings.TRANSMISSION['default']['HOST'],
+        port=settings.TRANSMISSION['default']['PORT'],
+        user=settings.TRANSMISSION['default']['USER'],
+        password=settings.TRANSMISSION['default']['PASSWORD'])
 
-    for hardlink in expired:
-        hardlink.delete()
-        unlink(
-            path.join(
-                settings.HARDLINK_PATH,
-                hardlink.token
-            )
+    filter = request.GET.get('filter', '')
+
+    rpclist = tc.get_torrents()
+    data = {}
+    for t in rpclist:
+        torrent, created = Torrent.objects.get_or_create(
+            hash=t.hashString
         )
+        if created:
+            torrent.name = t.name
+            torrent.save()
 
-    file = get_object_or_404(File, pk=file)
+        data[t.hashString] = {
+            'hash': t.hashString,
+            'name': t.name,
+            'status': t.status,
+            'progress': t.progress,
+            'recheckProgress': t.recheckProgress * 100,
+        }
 
-    token = sha1('%s:%s:%s' % (
-        random(),
-        file.pk,
-        file.filename
-    )).hexdigest()
-
-    hardlink = Hardlink(
-        token=token,
-        file=file,
-        user=request.user
-    )
-    hardlink.save()
-
-    symlink(
-        '%s/%s' % (
-            settings.VAULT_PATH,
-            file.filename
-        ),
-        '%s/%s' % (
-            settings.HARDLINK_PATH,
-            token
-        )
-    )
+    for t in Torrent.objects.all():
+        try:
+            data[t.hash]['name'] = t.name
+        except KeyError:
+            t.delete()
+            del(data[t.hash])
 
     return HttpResponse(
-        content=dumps({
-            'status': 'ok',
-            'token': token
-        }), content_type='application/json'
+        content=dumps(data),
+        content_type='application/json'
     )
+
 
 def download(request, hardlink):
     """
@@ -324,6 +273,48 @@ def file(request, file):
 
 
 @login_required
+def hardlink(request, file):
+    """
+    Create hardlink to file
+    """
+    Hardlink.objects.filter(
+        created__lte = (datetime.now() - timedelta(
+            seconds=settings.HARDLINK_TTL))).delete()
+
+    file = get_object_or_404(File, pk=file)
+
+    token = sha1('%s:%s:%s' % (
+        random(),
+        file.pk,
+        file.filename
+    )).hexdigest()
+
+    hardlink = Hardlink(
+        token=token,
+        file=file,
+        user=request.user
+    )
+    hardlink.save()
+
+    redirect = request.GET.get('redirect')
+
+    if redirect == 1:
+        return redirect(
+            path.join(
+                settings.INTERNAL_DOWNLOAD_PATH,
+                token
+            )
+        )
+
+    return HttpResponse(
+        content=dumps({
+            'status': 'ok',
+            'token': token
+        }), content_type='application/json'
+    )
+
+
+@login_required
 def index(request):
     """
     Index page
@@ -345,15 +336,15 @@ def view_login(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return redirect('/')
-            else:
-                pass
-                # Return a 'disabled account' error message
-        else:
-            pass
-            # Return an 'invalid login' error message.
+                next_url = request.GET.get('next', '/')
+                return redirect(next_url)
+            #else:
+            #    pass
+            #    # Return a 'disabled account' error message
+        #else:
+        #    pass
+        #    # Return an 'invalid login' error message.
     else:
-        pass
         return render_to_response(
             'transmission/login.html',
             context_instance=RequestContext(request))
