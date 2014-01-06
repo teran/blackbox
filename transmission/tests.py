6aeea60c8cf17bbcae8849ab5ec61a39f1fcf463
Igor Shishkin <me@teran.ru>
2014-01-06 06:19:47 +0400

    unit tests added for transmission app

--- transmission/tests.py
+++ transmission/tests.py
@@ -5,12 +5,99 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from transmission.models import Torrent, File, Hardlink, View
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class BlackboxUnitTest(TestCase):
+    def test_create_torrent(self):
+        torrent = Torrent(
+            hash='123',
+            name='test_torrent'
+        )
+        torrent.save()
+
+        torrent = Torrent.objects.get(hash='123')
+
+        self.assertEqual(torrent.hash, '123')
+        self.assertEqual(torrent.name, 'test_torrent')
+
+    def test_create_file(self):
+        torrent = Torrent(
+            hash='123',
+            name='test_torrent'
+        )
+        torrent.save()
+
+        file = File(
+            filename='test_filename.name',
+            torrent=torrent
+        )
+        file.save()
+
+        file = File.objects.get(
+            filename='test_filename.name',
+            torrent=torrent
+        )
+
+        self.assertEqual(file.filename, 'test_filename.name')
+        self.assertEqual(file.torrent.id, torrent.id)
+
+    def test_create_hardlink(self):
+        torrent = Torrent(
+            hash='123',
+            name='test_torrent'
+        )
+        torrent.save()
+
+        file = File(
+            filename='test_filename.name',
+            torrent=torrent
+        )
+        file.save()
+
+        user = User(username='test_user')
+        user.save()
+
+        hardlink = Hardlink(
+            token='123456',
+            file=file,
+            user=user
+        )
+        hardlink.save()
+
+        self.assertEqual(hardlink.token, '123456')
+        self.assertEqual(hardlink.file, file)
+        self.assertEqual(hardlink.user, user)
+
+    def test_create_view(self):
+        torrent = Torrent(
+            hash='123',
+            name='test_torrent'
+        )
+        torrent.save()
+
+        file = File(
+            filename='test_filename.name',
+            torrent=torrent
+        )
+        file.save()
+
+        user = User(username='test_user')
+        user.save()
+
+        view = View(
+            file=file,
+            user=user
+        )
+        view.save()
+
+        view = View.objects.get(
+            file=file,
+            user=user
+        )
+
+        self.assertEqual(view.file, file)
+        self.assertEqual(view.user, user)
